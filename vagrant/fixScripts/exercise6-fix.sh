13846ac57792f2e1e209825ab5999b19f217426b
bennymestel <56840166+bennymestel@users.noreply.github.com>
2020-03-05 16:35:41 +0200

    Update exercise6-fix.sh

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+
+declare bytes = 0
+
+# Finds if running server is server1 or server2
+hs = 'hostname'
+
+for last; do true; done
+LAST_PATH = $last
+
+# Goes through the input array and copies each file to the destination path
+for FILE in "$@"
+do
+	$bytes += wc -c $FILE | awk '{print $1}'
+	
+	# Copies the file to the other server
+	if [ $hs == 'server1' ]
+	then	
+		scp -3 root@server1:$FILE root@server2:$LAST_PATH
+	else
+		scp -3 root@server2:$FILE root@server1:$LAST_PATH
+	fi	
+done
+
+echo "$bytes"
